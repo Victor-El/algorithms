5720bf73c1b8c486e377e7bab89fb4cc7cb7ca26
Victor-El <victorelezua@gmail.com>
2020-06-21 10:33:21 +0100

    done: done with binry search

--- binary-search.py
+++ binary-search.py
@@ -21,6 +21,8 @@ print("Binary Search")
 t1 = 0
 t2 = 0
 
+steps = 0
+
 """
 The generation of this array takes a lot of time
 When you run this program don't look at the time in 
@@ -29,23 +31,48 @@ the program itself. Compare with the time of linear search.
 You can increase or reduce the number of elements or change
 you search number.
 """
-arg_arr = list(map(lambda x: x, range(100_000_000)))
+arg_arr = list(map(lambda x: x, range(1_000_000)))
 
-def search(arr, num):
+t1 = 0
+t2 = 0
+steps = 0
+
+def search(arr, key):
 	global t1
 	global t2
+	global steps
 
 	t1 = time.time()
 	arr.sort() # This step is neccessary for binary search to work
 
-	for i in arr:
-		if i == num:
-			t2 = time.time()
-			return arr.index(i)
+	start_index = 0
+	end_index = len(arr) - 1 
+	mid_index = (start_index + end_index + 1) // 2 # using integer division here to avoid gettting a float which can cause an exception
+	element = None
+
+	if key > arr[end_index]:
+		t2 = time.time()
+		return element
+
+
+	while element == None and end_index <= end_index:
+		steps += 1
+
+		if key == arr[mid_index]:
+			element = arr[mid_index]
+		if key > arr[mid_index]:
+			start_index = mid_index + 1
+			mid_index = (start_index + end_index + 1) // 2
+		if key < arr[mid_index]:
+			end_index = mid_index - 1
+			mid_index = (start_index + end_index + 1) // 2
 
 	t2 = time.time()
-	return None
+	return element
 
-result = search(arg_arr, 5_000_000)
+	
+
+result = search(arg_arr, 1_000_000)
 
 print("found result: ", result, "after ", t2 - t1, "secs")
+print("In", steps, "steps")
