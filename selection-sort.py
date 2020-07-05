db249d0910bae7639ad2459452f2aab59e26529e
Victor-El <victorelezua@gmail.com>
2020-07-05 13:49:43 +0100

    correct selection sort

--- selection-sort.py
+++ selection-sort.py
@@ -4,12 +4,26 @@
  inner loop is responsible for doing the comparison and swapping of values.
 """
 
-def sort(arr):
-	for i in range(len(arr)):
-		for j in range(len(arr)):
-			if arr[i] < arr[j]:
-				arr[i], arr[j] = arr[j], arr[i]
+# def sort(arr):
+# 	for i in range(len(arr)):
+# 		for j in range(len(arr)):
+# 			if arr[i] < arr[j]:
+# 				arr[i], arr[j] = arr[j], arr[i]
 
+# 	print(arr)
+
+def sort(arr):
 	print(arr)
+
+	for i in range(len(arr)):
+		min = i
+
+		for j in range(i + 1, len(arr)):
+			if arr[min] > arr[j]:
+				min = j
+
+		arr[i], arr[min] = arr[min], arr[i]
+
+	print("Sorted: ", arr)
 
 sort([2, 5,1,0,5,73,87,3,98,0,34,8])
